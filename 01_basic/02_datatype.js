6679e81836cf822988f52e21d3e832428118b857
Akshep Yadav <94985630+akshep@users.noreply.github.com>
2024-02-28 10:49:49 +0000

    container config done

--- 01_basic/02_datatype.js
+++ 01_basic/02_datatype.js
@@ -1,1 +1,20 @@
- 
+ "use strict"; // treat all JS code as newer version
+
+ // alert (3+3) // we are using nodejs, not browserr
+
+ console.log(3 + 3);
+
+ console.log("Akshep");
+
+ // number => 2 to power 53
+ // bigint 
+ // string => ""
+ // boolean => true/false
+ // null=> standalone value
+ // undefined
+ // symbol => uniqre
+
+ // object
+
+  console.log(typeof undefined); // undefined
+  console.log(typeof null); //object
